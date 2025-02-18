d2e8f9bce75b00a4e540ea13a8d904b62d05d809
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-02-18 12:52:02 -0300

    Update Song.jsx

--- front-end/src/pages/Song.jsx
+++ front-end/src/pages/Song.jsx
@@ -5,50 +5,52 @@ import { Link, useParams } from 'react-router-dom'
 import { songsArray } from '../assets/database/songs'
 import { artistArray } from '../assets/database/artists'
 const Song = () => {
-  const {id} = useParams()
-  const {image, name, duration, artist, audio, index} = songsArray.filter(
-    (current) => current._id === id
-  )[0]
-  const artistOb = artistArray.filter(
-    (currentArt) => currentArt.name === artist
-  )[0]
-  const songObj = songsArray.filter(currentSong => currentSong._id === id)
+  const { id } = useParams();
+  const songIndex = songsArray.findIndex((song) => song._id === id);
+  const songObj = songsArray[songIndex];
+  const artistOb = artistArray.find((artist) => artist.name === songObj.artist);
+  
   return (
     <>
       <section className="song__item flex column">
         <div className="container">
-          <img src={image} alt={`capa música ${name}`}/>
+          <img src={songObj.image} alt={`capa música ${songObj.name}`} />
         </div>
         <div className="flex play__details">
           <Link to={`/artist/${artistOb._id}`} className="song__album">
-            <img src={artistOb.image} alt={artistOb.name}/>
+            <img src={artistOb.image} alt={artistOb.name} />
           </Link>
           <div className="player flex column">
-              <div className="buttons flex">
-                <Link to={`/song/${songObj[0].id - 1}`} className="prev">
-                  <FontAwesomeIcon icon={faBackward} />
-                </Link>
-                <span className="play">
-                  <FontAwesomeIcon icon={faPlay} />
-                </span>
-                <Link to={`/song/${songObj[0].id + 1}`} className="next">
-                  <FontAwesomeIcon icon={faForward} />
-                </Link>
-              </div>
-              <div className="bar__progress flex">
-                <span className="music__initial">00:00</span>
-                <div className="bar"></div>
-                <span className="music__time">{duration}</span>
-              </div>
+            <div className="buttons flex">
+              <Link 
+                to={songIndex > 0 ? `/song/${songsArray[songIndex - 1]._id}` : '#'} 
+                className="prev"
+              >
+                <FontAwesomeIcon icon={faBackward} />
+              </Link>
+              <span className="play">
+                <FontAwesomeIcon icon={faPlay} />
+              </span>
+              <Link 
+                to={songIndex < songsArray.length - 1 ? `/song/${songsArray[songIndex + 1]._id}` : '#'} 
+                className="next"
+              >
+                <FontAwesomeIcon icon={faForward} />
+              </Link>
+            </div>
+            <div className="bar__progress flex">
+              <span className="music__initial">00:00</span>
+              <div className="bar"></div>
+              <span className="music__time">{songObj.duration}</span>
+            </div>
           </div>
-          <div clasName="song__name">
-            <h3>{name}</h3>
-            <h4>{artist}</h4>
+          <div className="song__name">
+            <h3>{songObj.name}</h3>
+            <h4>{songObj.artist}</h4>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
-
-export default Song
+export default Song;
