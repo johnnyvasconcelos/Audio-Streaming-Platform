a40294f3aaaf6729bc4ba5c4491a49b6028fbc34
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-02-18 11:17:02 -0300

    passando informações dos arrays em arquivos para o mongodb
    comando executado por node enviando informações para o mongodb

---
+++ back-end/api/insertMany.js
@@ -0,0 +1,20 @@
+import { songsArray } from '../../front-end/src/assets/database/songs.js';
+import { artistArray } from '../../front-end/src/assets/database/artists.js';
+import { db } from './connect.js'
+const newArtistArray = artistArray.map((artist) => {
+    const newArtistObj = { ...artist}
+    delete newArtistObj.id
+    return newArtistObj
+})
+
+const newSongArray = songsArray.map((song) => {
+    const newSongObj = { ...song}
+    delete newSongObj.id
+    return newSongObj
+})
+
+const responseSong = await db.collection('songs').insertMany(newSongArray)
+const responseArtist = await db.collection('artists').insertMany(newArtistArray)
+
+console.log(responseArtist)
+console.log(responseSong)
--- back-end/api/server.js
+++ back-end/api/server.js
@@ -2,12 +2,16 @@ import express from 'express';
 const App = express();
 const PORT = 3421;
 import { songsArray } from '../../front-end/src/assets/database/songs.js';
+import { db } from './connect.js'
 App.listen(PORT, () => {
     console.log('Servidor está na porta ' + PORT)
 });
 App.get('/', (req, res) => {
     res.send(songsArray)
 });
-App.get('/artists', (req, res) => {
-    res.send('OK')
+App.get('/artists', async (req, res) => {
+    res.send(await db.collection('artists').find({}).toArray())
+});
+App.get('/songs', async (req, res) => {
+    res.send(await db.collection('songs').find({}).toArray())
 });
