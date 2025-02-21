ff44ca6ff97c31e4b04744303a6057716fbd5991
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-02-21 14:11:00 -0300

    correções await async

--- front-end/src/pages/Artist.jsx
+++ front-end/src/pages/Artist.jsx
@@ -1,39 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
-import SongList from '../components/SongList.jsx'
-import { Link, useParams } from 'react-router-dom'
-import { artistArray } from '../assets/database/artists.js'
-import { songsArray } from '../assets/database/songs.js'
+import SongList from '../components/SongList.jsx';
+import { Link, useParams } from 'react-router-dom';
+import { artistArray } from '../assets/database/artists.js';
+import { songsArray } from '../assets/database/songs.js';
 const Artist = () => {
-  const { id } = useParams()
-  const artId = artistArray.filter((currentArtist, index) => currentArtist._id === id)[0]
-  const songsArtist = songsArray.filter((currentSong, index) => currentSong.artist === artId.name)
-  const randomIndex = Math.floor(Math.random() * (songsArray.length - 1))
-  const randomId = songsArray[randomIndex]._id
-  console.log(artId.banner + " Banner.webp")
+  const { id } = useParams();
+  const [artId, setArtId] = useState(null);
+  const [songsArtist, setSongsArtist] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchArtistData = async () => {
+      const artist = artistArray.filter((currentArtist) => currentArtist._id === id)[0];
+      if (artist) {
+        setArtId(artist);
+        const songs = songsArray.filter((currentSong) => currentSong.artist === artist.name);
+        setSongsArtist(songs);
+      } else {
+        console.error('Artista não encontrado');
+      }
+      setIsLoading(false);
+    };
+    fetchArtistData();
+  }, [id]);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (!artId) {
+    return <div>Artista não encontrado</div>;
+  }
+  const randomIndex = Math.floor(Math.random() * (songsArtist.length - 1));
+  const randomId = songsArtist[randomIndex]._id;
   return (
-    <>
-      <section className="artist">
-        <div className="artist__header" style={{
-    backgroundImage: `url('/src/assets/images/artists/${artId.banner} Banner.webp'), 
-                      linear-gradient(to bottom, var(--Dark), var(--DarkBlue), var(--DarkPurple))`,
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-}}>
-          <div className="container">
-            <h1 className="artist__title">{artId.name}</h1>
-          </div>
+    <section className="artist">
+      <div
+        className="artist__header"
+        style={{
+          backgroundImage: `url('/src/assets/images/artists/${artId.banner} Banner.webp'),
+            linear-gradient(to bottom, var(--Dark), var(--DarkBlue), var(--DarkPurple))`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="container">
+          <h1 className="artist__title">{artId.name}</h1>
         </div>
-        <div className="artist__body space-page container">
-          <h2>Popular</h2>
-          <SongList songs={songsArtist} />
-        </div>
-        <Link to={`/song/${randomId}`}>
+      </div>
+      <div className="artist__body space-page container">
+        <h2>Popular</h2>
+        <SongList songs={songsArtist} />
+      </div>
+      <Link to={`/song/${randomId}`}>
         <FontAwesomeIcon icon={faCirclePlay} className="music__item--icon music-item__icon--artist" />
-        </Link>
-      </section>
-    </>
-  )
+      </Link>
+    </section>
+  );
 };
-export default Artist
+export default Artist;
