4be63c3cab5e42fbd4773531ea50315b31cec24e
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-02-21 13:37:54 -0300

    ajustes back-end

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import 'dotenv/config'
-const NODE_ENV = process.env.NODE_ENV
-const URI = NODE_ENV === 'https://audio-streaming-platform.onrender.com/api/'
+//const NODE_ENV = process.env.NODE_ENV
+const URI = 'https://audio-streaming-platform.onrender.com/api/'
 const responseArtists = await axios.get(URI + '/artists')
 const responseSongs = await axios.get(URI + '/songs')
 export const artistArray = responseArtists.data
